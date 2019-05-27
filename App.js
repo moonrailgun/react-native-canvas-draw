176ca8716e84b204e3708bf747d5b79b04227716
moonrailgun <moonrailgun@gmail.com>
2019-05-27 18:26:31 +0800

    init react-native wrap and copy from react-canvas-draw

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import CanvasDraw from './src/CanvasDraw';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Demo</Text>
+        <View style={{ borderWidth: 1 }}>
+          <CanvasDraw />
+        </View>
       </View>
     );
   }
